163b5d3508b225cdc6c0213543dd964d8c8bcc91
pesaravai-manohar <pesaravaimanohar@gmail.com>
2026-08-19 20:21:16 +0530

    used HashMap

--- containsDuplicates_217.java
+++ containsDuplicates_217.java
@@ -1,14 +1,10 @@
 import java.util.*;
 public class containsDuplicates_217{
     public boolean containsDuplicate(int[] nums) {
-        Map<Integer,Integer> numbers = new HashMap<>();
-        for(int i=0;i<nums.length;i++){
-            if(numbers.containsKey(nums[i])){
+        Set<Integer> numbers = new HashSet<>();
+        for(int x:nums){
+            if(!numbers.add(x))
                 return true;
-            }
-            else{
-                numbers.put(nums[i],1);
-            }
         }
         return false;
     }

c2a0e707c253a1727dc845c0d7a722efbbea754a
pesaravai-manohar <pesaravaimanohar@gmail.com>
2026-08-19 20:08:52 +0530

    using HashMap

---
+++ containsDuplicates.java
@@ -0,0 +1,14 @@
+public class containsDuplicates{
+    public boolean containsDuplicate(int[] nums) {
+        Map<Integer,Integer> numbers = new HashMap<>();
+        for(int i=0;i<nums.length;i++){
+            if(numbers.containsKey(nums[i])){
+                return true;
+            }
+            else{
+                numbers.put(nums[i],1);
+            }
+        }
+        return false;
+    }
+}

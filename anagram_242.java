1c687248aaa810f81554da747f5d7a49373d2edc
pesaravai-manohar <pesaravaimanohar@gmail.com>
2026-08-20 17:04:33 +0530

    optimised->runtime 31ms to 12s

--- anagram_242.java
+++ anagram_242.java
@@ -1,31 +1,31 @@
 import java.util.*;
 public class anagram_242{
-    public boolean isAnagram(String s, String t) {
+   public boolean isAnagram(String s, String t) {
         Map<Character,Integer> s1 = new HashMap<>();
-        Map<Character,Integer> s2 = new HashMap<>();
         if(s.length()!=t.length()){
             return false;
         }
             for(int i=0;i<s.length();i++){
                 char c = s.charAt(i);
-                char d = t.charAt(i);
-                if(s1.containsKey(c)){
-                    s1.put(c,s1.get(c)+1);
+                if(!s1.containsKey(c)){
+                int count1 =0;
+                int count2=0;
+                    for(int j=0;j<s.length();j++){
+                        if(s.charAt(j)==c){
+                            count1++;
+                        }
+                        if(t.charAt(j)==c){
+                            count2++;
+                        }
+                    }
+                    if(count1!=count2){
+                        return false;
+                    }
+                    else{
+                        s1.put(c,count1);
+                    }
                 }
-                else{
-                    s1.put(c,1);
-                }
-                if(s2.containsKey(d)){
-                    s2.put(d,s2.get(d)+1);
-                }
-                else{
-                    s2.put(d,1);
-                }
-
             }
-            if(s1.equals(s2)){
-                return true;
-            }
-    return false;
+    return true;
     }
 }
